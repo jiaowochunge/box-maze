e241ab8adb8c3304052915e39d2d7d3fb2d19e17
john.mac <john@johndeMacBook-Pro.local>
2020-04-10 22:31:01 +0800

    两个页面整合到一起

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MapEditor } from 'components/MapEditor'
+import { Game } from 'components/Game'
 
 import 'assets/css/game.css'
 
 export const Home = () => {
+  const [tab, setTab] = useState<number>(1)
+
   return (
     <div>
-      <MapEditor />
+      <div>
+        <button onClick={() => setTab(1)}>demo试玩</button>
+        <button onClick={() => setTab(2)}>地图编辑器</button>
+      </div>
+      <div style={{display: tab == 1 ? 'block' : 'none'}}>
+        <Game level={1} onSuccess={() => alert('you win')} />
+      </div>
+      <div style={{display: tab == 2 ? 'block' : 'none'}}>
+        <MapEditor onSaveMap={(map) => {console.log(map)}} />
+      </div>
     </div>
   )
 }
